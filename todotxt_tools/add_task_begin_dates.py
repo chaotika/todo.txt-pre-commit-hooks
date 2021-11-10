a2bd333e5c91fcd14e69b10b100dee23fbb1d86a
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2021-11-10 16:46:06 +0100

    add_task_begin_dates works

--- todotxt_tools/add_task_begin_dates.py
+++ todotxt_tools/add_task_begin_dates.py
@@ -3,6 +3,23 @@ import argparse
 from typing import Optional
 from typing import Sequence
 
+import pytodotxt
+import datetime
+
+def add_task_begin_dates(todotxt_filename):
+    print(todotxt_filename)
+
+    todotxt = pytodotxt.TodoTxt(todotxt_filename)
+    todotxt.parse()
+
+    today = datetime.date.today()
+
+    for task in todotxt.tasks:
+        if not task.creation_date:
+            task.creation_date = today
+
+    todotxt.save()
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -15,13 +32,19 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     )
     args = parser.parse_args(argv)
 
+    changed = False
+    for filename in args.filenames:
+        if add_task_begin_dates(filename):
+            changed = True
     # return find_large_added_files(
     #     args.filenames,
     #     args.maxkb,
     #     enforce_all=args.enforce_all,
     # )
-
-    return 0
+    if changed:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
     raise SystemExit(main())
